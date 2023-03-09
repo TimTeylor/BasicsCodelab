dd0972d909b75c31cdf1241b581a22caa1f01a4e
Pavel Dumanskii <timteillor@gmail.com>
2023-03-10 02:08:22 +0400

    Add new color scheme

--- app/src/main/java/com/example/basicscodelab/ui/theme/Theme.kt
+++ app/src/main/java/com/example/basicscodelab/ui/theme/Theme.kt
@@ -10,21 +10,36 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.ViewCompat
 
 private val DarkColorScheme = darkColorScheme(
+    surface = Blue,
+    onSurface = Navy,
+    primary = Navy,
+    onPrimary = Chartreuse
+
+    /* Default color
     primary = Purple80,
     secondary = PurpleGrey80,
     tertiary = Pink80
+     */
 )
 
 private val LightColorScheme = lightColorScheme(
+    surface = Blue,
+    onSurface = Color.White,
+    primary = LightBlue,
+    onPrimary = Navy
+
+    /* Default color
     primary = Purple40,
     secondary = PurpleGrey40,
     tertiary = Pink40
+     */
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
